d60657d519142f51cc5284a1ccdeb9cc5387cf05
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-07-28 21:28:17 -0500

    interview

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#Write a method that determines if all the boxes can be opened.
+
+
+def canUnlockAll(boxes):
+
+    keybox = [0]
+
+    for box in keybox:
+        for key in boxes[box]:
+            if key not in keybox and key < len(boxes):
+                keybox.append(key)
+
+    if len(keybox) == len(boxes):
+        return (True)
+    else:
+        return (False)
