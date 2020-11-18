db5b08053d3cf0540e8c7aa8695fbf7b6d11ae97
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-11-18 13:21:25 -0500

    interview

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
     const mylist = JSON.parse(body).characters;
-    let i = 0;
-    for (i in mylist) {
-      request(mylist[i], function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+    for (const i in mylist) {
+      const req = new Promise((resolve, reject) => {
+        request(mylist[i], function (err, response, body) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(body);
+          }
+        });
       });
+      await req.then(body => console.log(JSON.parse(body).name)).catch(err => console.log(err));
     }
   }
 });
