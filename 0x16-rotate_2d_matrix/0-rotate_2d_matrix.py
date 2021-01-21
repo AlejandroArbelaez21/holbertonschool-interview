ca3a6c7806b432dff58e82616f463f4d1cfdd42b
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2021-01-20 21:06:10 -0500

    interview

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Test 0x16 - Rotate 2D Matrix
+"""
+def rotate_2d_matrix(matrix):
+    """ """
+    matrix_copy = matrix.copy()
+
+    for i in range(len(matrix[0])):
+        temp_array = []
+        for row in reversed(matrix_copy):
+            temp_array.append(row[i])
+        matrix[i] = temp_array
