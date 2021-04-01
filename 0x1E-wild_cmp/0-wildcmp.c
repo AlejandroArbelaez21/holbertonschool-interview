b0f6dfd5ecc24b3e3813a894aa811bd20c94dc55
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2021-03-31 22:35:31 -0500

    interview

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * wildcmp - compares two strings
+ * @s: ptr to string 1
+ * @s2: ptr to string 2 (may contain wildcard denoted by '*')
+ *
+ * Return: 1 if identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+	return (0);
+}
