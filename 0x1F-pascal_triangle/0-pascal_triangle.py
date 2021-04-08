e00aa31afe46f22e80e76dc6b7684fbc0c88415c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2021-04-07 21:21:42 -0500

    interview

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers representing Pascal’s triangle """
+    result = []
+    if n <= 0:
+        return result
+    else:
+        for count in range(n):
+            row = [1]
+            for element in range(count):
+                row.append(sum(result[-1][element:element+2]))
+            result.append(row)
+        return result
+
