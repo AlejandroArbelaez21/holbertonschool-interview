7b96cce197ccf9838bd80eafcdb077ab103a1d30
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2021-04-07 19:43:36 -0500

    fixing up the function

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-import math
 """
 0. Pascal's Triangle
 """
-
-
-def combination(n, r):
-    """ correct calculation of combinations, n choose k """ 
-    return int((math.factorial(n)) / ((math.factorial(r)) * math.factorial(n - r)))
 
 
 def pascal_triangle(n):
@@ -17,9 +11,9 @@ def pascal_triangle(n):
         return result
     else:
         for count in range(n): 
-            row = []
-            for element in range(count + 1): 
-                row.append(combination(count, element))
+            row = [1]
+            for element in range(count): 
+                row.append(sum(result[-1][element:element+2]))
             result.append(row)
         return result
     
